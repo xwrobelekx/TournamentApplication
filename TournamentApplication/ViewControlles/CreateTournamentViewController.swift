58f10cc90d34a1c8ccb9b2d0cde94e6f53d79bf2
Kamil Wrobel <wrobelk@live.com>
2018-11-06 15:34:05 -0700

    Basic design for the add players view, connected all the outlets and actions

---
+++ TournamentApplication/ViewControlles/CreateTournamentViewController.swift
@@ -0,0 +1,59 @@
+//
+//  CreateTournamentViewController.swift
+//  TournamentApplication
+//
+//  Created by Kamil Wrobel on 11/6/18.
+//  Copyright © 2018 Kamil Wrobel. All rights reserved.
+//
+
+import UIKit
+
+class CreateTournamentViewController: UIViewController {
+
+    //MARK: - Outlets
+    @IBOutlet weak var tournamentNameTextField: UITextField!
+    @IBOutlet weak var playerNameTextField: UITextField!
+    @IBOutlet weak var playersTableView: UITableView!
+    
+    
+    //MARK: - LifeCycle Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+    //MARK: - Actions
+    @IBAction func rearangePlayersButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func shufflePlayersButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func fourPlayersButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func eightPlayersButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func sixteenPlayersButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func thirtytwoPlayersButtonTapped(_ sender: Any) {
+    }
+    
+    
+    @IBAction func addPlayerButtonTapped(_ sender: Any) {
+    }
+    
+    
+    
+    @IBAction func saveButtonTapped(_ sender: Any) {
+    }
+    
+    
+    
+    
+
+
+}
